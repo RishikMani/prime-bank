d6f03c549650c90d7e5ff7f1383bbe52a257ed49
Rishik Mani <r.mani@parityqc.com>
2026-08-22 23:36:20 +0200

    update some field names and defaults, add is_active flag

--- src/branch/models.py
+++ src/branch/models.py
@@ -2,12 +2,13 @@ from django.db import models
 
 
 class Branch(models.Model):
-    id = models.SmallIntegerField(primary_key=True)
-    name = models.CharField(max_length=50)
+    id = models.SmallIntegerField(primary_key=True, db_column="branch_id")
+    branch_name = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
-    opened_date = models.DateField()
+    opening_date = models.DateField()
     ifsc = models.CharField(max_length=12)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         db_table = "branch"
